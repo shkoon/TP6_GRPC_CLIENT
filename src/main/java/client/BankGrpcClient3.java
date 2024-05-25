45717f20b0243d4952813614c22587168db256d0
shkoon <hafde79@gmail.com>
2024-05-25 21:49:25 +0000

    first commit

---
+++ src/main/java/client/BankGrpcClient3.java
@@ -0,0 +1,44 @@
+package client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import ma.enset.stubs.Bank;
+import ma.enset.stubs.BankServiceGrpc;
+
+import java.io.IOException;
+
+public class BankGrpcClient3 {
+    public static void main(String[] args) throws IOException {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",5555)
+                .usePlaintext()
+                .build();
+        BankServiceGrpc.BankServiceStub asyncStub = BankServiceGrpc.newStub(managedChannel);
+        Bank.ConvertCurrencyRequest request= Bank.ConvertCurrencyRequest.newBuilder()
+                .setCurrencyFrom("MAD")
+                .setCurrencyTo("USD")
+                .setAmount(6500)
+                .build();
+
+        asyncStub.getCurrencyStream(request, new StreamObserver<Bank.ConvertCurrencyResponse>() {
+            @Override
+            public void onNext(Bank.ConvertCurrencyResponse convertCurrencyResponse) {
+                System.out.println("-----------");
+                System.out.println(convertCurrencyResponse);
+                System.out.println("-----------");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("END");
+            }
+        });
+        System.out.println("...START");
+        System.in.read();
+    }
+}
---
+++ src/main/java/client/BankGrpcClient4.java
@@ -0,0 +1,63 @@
+package client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import ma.enset.stubs.Bank;
+import ma.enset.stubs.BankServiceGrpc;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class BankGrpcClient4 {
+    public static void main(String[] args) throws IOException {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",5555)
+                .usePlaintext()
+                .build();
+        BankServiceGrpc.BankServiceStub asyncStub = BankServiceGrpc.newStub(managedChannel);
+        Bank.ConvertCurrencyRequest request= Bank.ConvertCurrencyRequest.newBuilder()
+                .setCurrencyFrom("MAD")
+                .setCurrencyTo("USD")
+                .setAmount(6500)
+                .build();
+
+        StreamObserver<Bank.ConvertCurrencyRequest> performStream = asyncStub.performStream(new StreamObserver<Bank.ConvertCurrencyResponse>() {
+            @Override
+            public void onNext(Bank.ConvertCurrencyResponse convertCurrencyResponse) {
+                System.out.println("+++++++++++");
+                System.out.println(convertCurrencyResponse);
+                System.out.println("+++++++++++");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("END");
+            }
+        });
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            int counter=0;
+            @Override
+            public void run() {
+                Bank.ConvertCurrencyRequest currencyRequest= Bank.ConvertCurrencyRequest.newBuilder()
+                        .setAmount(Math.random()*7000)
+                        .build();
+                performStream.onNext(currencyRequest);
+                System.out.println("=======> counter : "+counter);
+                ++counter;
+                if(counter==20){
+                    performStream.onCompleted();
+                    timer.cancel();
+                }
+            }
+        }, 1000, 1000);
+        System.out.println("...START");
+        System.in.read();
+    }
+}
